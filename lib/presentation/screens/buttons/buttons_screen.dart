b2e2403bbd73faf5bc0962fd61074f92a67c90f3
Andres Rivero <andres19rivero@gmail.com>
2026-03-02 17:29:34 -0400

    feat(screens): add several button types to ButtonsScreen

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const String name = 'buttons_screen';
@@ -9,7 +10,74 @@ class ButtonsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Buttons Screen')),
-      body: Placeholder(),
+      body: _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_back_ios_rounded),
+        onPressed: () {
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          runSpacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text('Elevated')),
+            ElevatedButton(onPressed: null, child: Text('Elevated Disabled')),
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: Text('Elevated Icon'),
+              icon: Icon(Icons.access_alarm_rounded),
+            ),
+            FilledButton(onPressed: () {}, child: Text('Filled')),
+            FilledButton.icon(
+              onPressed: () {},
+              label: Text('Filled Icon'),
+              icon: Icon(Icons.accessibility_new),
+            ),
+            OutlinedButton(onPressed: () {}, child: Text('Outlined')),
+            OutlinedButton.icon(
+              onPressed: () {},
+              label: Text('Outlined Icon'),
+              icon: Icon(Icons.terminal),
+            ),
+            TextButton(onPressed: () {}, child: Text('Text')),
+            TextButton.icon(
+              onPressed: () {},
+              label: Text('Text Icon'),
+              icon: Icon(Icons.account_box_outlined),
+            ),
+            // TODO: custom button
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.app_registration_rounded),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.app_registration_rounded),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(colors.primary),
+                iconColor: WidgetStatePropertyAll(Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
