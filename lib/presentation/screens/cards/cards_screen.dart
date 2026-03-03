8f796fa83118d0c1071e17b087e7c56dcf908bc5
Andres Rivero <andres19rivero@gmail.com>
2026-03-02 21:08:01 -0400

    feat(cards): implement CardType2 with custom border

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -35,6 +35,10 @@ class _CardsView extends StatelessWidget {
             (card) =>
                 _CardType1(label: card['label'], elevation: card['elevation']),
           ),
+          ...cards.map(
+            (card) =>
+                _CardType2(label: card['label'], elevation: card['elevation']),
+          ),
         ],
       ),
     );
@@ -63,6 +67,44 @@ class _CardType1 extends StatelessWidget {
               ),
             ),
             Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        side: BorderSide(color: colors.outline),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: EdgeInsetsGeometry.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
+            ),
           ],
         ),
       ),
