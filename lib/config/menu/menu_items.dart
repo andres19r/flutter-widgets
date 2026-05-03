b2fe8f00cea826129844ce586aa2cd657c8fa88a
Andres Rivero <andres19rivero@gmail.com>
2026-05-02 22:46:09 -0400

    feat(tutorial): implement app_tutorial screen with basic slides

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -51,4 +51,10 @@ const appMenuItems = <MenuItem>[
     link: '/ui-controls',
     icon: Icons.car_rental_outlined,
   ),
+  MenuItem(
+    title: 'Application tutorial',
+    subTitle: 'Small intro tutorial',
+    link: '/tutorial',
+    icon: Icons.accessible_rounded,
+  ),
 ];
--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo>[
+  SlideInfo('Search the food', 'caption', 'assets/images/1.png'),
+  SlideInfo('Fast delivery', 'caption', 'assets/images/2.png'),
+  SlideInfo('Enjoy the food', 'caption', 'assets/images/3.png'),
+];
 
 class AppTutorialScreen extends StatelessWidget {
   static const name = 'tutorial_screen';
@@ -7,6 +22,67 @@ class AppTutorialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            physics: BouncingScrollPhysics(),
+            children: slides
+                .map(
+                  (slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl,
+                  ),
+                )
+                .toList(),
+          ),
+          Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(
+              child: Text('Skip'),
+              onPressed: () => context.pop(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: AssetImage(imageUrl)),
+            SizedBox(height: 20),
+            Text(title, style: titleStyle),
+            SizedBox(height: 10),
+            Text(caption, style: captionStyle),
+          ],
+        ),
+      ),
+    );
   }
 }
