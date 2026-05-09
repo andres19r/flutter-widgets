3f1836c4c816a83dd84d377969fe65a3e1b3f954
Andres Rivero <andres19rivero@gmail.com>
2026-05-08 23:02:38 -0400

    feat(counter): enhance counter screen with dark mode toggle and type safety

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
@@ -10,9 +11,22 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
 
     return Scaffold(
-      appBar: AppBar(title: Text('Counter Screen')),
+      appBar: AppBar(
+        title: Text('Counter Screen'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.read(isDarkModeProvider.notifier).update((state) => !state);
+            },
+            icon: Icon(
+              isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined,
+            ),
+          ),
+        ],
+      ),
       body: Center(
         child: Text(
           'Valor: $clickCounter',
@@ -21,7 +35,10 @@ class CounterScreen extends ConsumerWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          // ref.read(counterProvider.notifier).state++;
+          ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
       ),
     );
   }
